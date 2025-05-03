a5efd64bbe3df57f6f2bc52a712af38fd37e12f5
Shabad <shabadmk969@outlook.com>
2025-05-03 21:50:42 +0530

    paginated-view-insertposts

--- myapp/views.py
+++ myapp/views.py
@@ -1,9 +1,38 @@
 from django.http import HttpResponse
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.contrib.auth.decorators import login_required
-
+from . models import Post
+from .forms import PostForm
+from django.contrib import messages
+from django.core.paginator import Paginator
 # Create your views here.
 
 @login_required
 def index(request):
-    return HttpResponse("Hello, world. You're at the index.")
+    user = request.user
+    query = Post.objects.filter(author=user)
+    paginator = Paginator(query, 4) 
+    page_number = request.GET.get("page")
+    posts = paginator.get_page(page_number)
+    
+    allPosts = Post.objects.exclude(author=user)
+    
+    context = {
+        'posts': posts,
+        'allPosts': allPosts
+    }
+    return render(request, 'myapp/dashboard.html', context)
+
+@login_required
+def createBlog(request):
+    if request.method == 'POST':
+        form = PostForm(request.POST)
+        if form.is_valid():
+            post = form.save(commit=False)
+            post.author = request.user
+            post.save()
+            messages.success(request, 'Blog created successfully')
+            return redirect('index')
+    else:
+        form = PostForm()
+    return render(request, 'myapp/createBlog.html', {'form': form})
